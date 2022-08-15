441387309c777bc0e0b5a83d799de3193c882ffb
Rosgrad <109528856+Rosgrad@users.noreply.github.com>
2022-08-16 00:07:22 +0300

    Add Binance

---
+++ Binance/Binance/Info.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binance
+{
+    public class Info
+    {
+        public  readonly HttpClient client = new HttpClient();
+      
+        public  async Task ProcessRepositories()
+        {
+            string result;
+            Console.WriteLine("Выберите возможный вариант: a:,b:,c");
+            result = Console.ReadLine();
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var Noparameter = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var symbol = client.GetStringAsync("https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC");
+            var symbols = client.GetStringAsync("https://api.binance.com/api/v3/exchangeInfo?symbols=%5B%22BNBBTC%22,%22BTCUSDT%22%5D" );
+           
+           
+            var msg1 = await symbol;
+            var msg2 = await symbols;
+
+
+            if (result == "a")
+            {
+                var msg = await Noparameter;
+                Console.Write(msg);
+            }
+            else if (result == "b")
+            {
+                Console.Write(msg1);
+            }
+            else if (result == "c")
+            {
+                Console.Write(msg2);
+            }
+
+
+
+        }
+    }
+}
---
+++ Binance/Binance/Program.cs
@@ -0,0 +1,23 @@
+﻿using Binance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+      
+        static async Task Main(string[] args)
+        {
+            Info info = new Info();
+            await info.ProcessRepositories();
+         
+        }
+
+
+    }
+}
